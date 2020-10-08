68cab85dc17382490ab0f39f7e39aef7e5e3b2d9
John Richmond Escalona <johnrichmondescalona@gmail.com>
2020-10-08 14:17:58 -0700

    Style Bars

--- src/components/common/bar/bar.jsx
+++ src/components/common/bar/bar.jsx
@@ -6,17 +6,35 @@ const Bar = ({ data }) => {
   const { nodeI, nodeJ } = data;
   const coord1 = calcRelativeCoord(nodeI.xCoord, nodeI.yCoord);
   const coord2 = calcRelativeCoord(nodeJ.xCoord, nodeJ.yCoord);
-
+  const colors = ["#9b9b9b","#5f5f5f","#333333"]
+  const widths = [12,7,2]
   return (
-    <line
-      className="bar"
-      x1={coord1.xRel}
-      y1={coord1.yRel}
-      x2={coord2.xRel}
-      y2={coord2.yRel}
-      stroke="#000000e0"
-      strokeWidth={4}
-    ></line>
+    <g className="bar">
+      <line
+        x1={coord1.xRel}
+        y1={coord1.yRel}
+        x2={coord2.xRel}
+        y2={coord2.yRel}
+        stroke={colors[0]}
+        strokeWidth={widths[0]}
+      />
+      <line
+        x1={coord1.xRel}
+        y1={coord1.yRel}
+        x2={coord2.xRel}
+        y2={coord2.yRel}
+        stroke={colors[1]}
+        strokeWidth={widths[1]}
+      />
+      <line
+        x1={coord1.xRel}
+        y1={coord1.yRel}
+        x2={coord2.xRel}
+        y2={coord2.yRel}
+        stroke={colors[2]}
+        strokeWidth={widths[2]}
+      />
+    </g>
   );
 };
 
