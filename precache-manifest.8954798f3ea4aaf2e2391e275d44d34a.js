9f79e60f9e467835157d4d3d73ce63c5d87021fc
John Richmond Escalona <johnrichmondescalona@gmail.com>
2020-11-03 16:26:16 -0800

    Updates

---
+++ precache-manifest.8954798f3ea4aaf2e2391e275d44d34a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263b6cedb7ea5c458704c1d650d3d85a",
+    "url": "/truss-solver/solver/index.html"
+  },
+  {
+    "revision": "ff5f923391fc4c4a7e59",
+    "url": "/truss-solver/solver/static/css/main.66ba8ebf.chunk.css"
+  },
+  {
+    "revision": "6de6ff77019f89dcfb0b",
+    "url": "/truss-solver/solver/static/js/2.35043cc4.chunk.js"
+  },
+  {
+    "revision": "fa3c65458c0dff53fb45377e9a84216e",
+    "url": "/truss-solver/solver/static/js/2.35043cc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff5f923391fc4c4a7e59",
+    "url": "/truss-solver/solver/static/js/main.d606c0a4.chunk.js"
+  },
+  {
+    "revision": "a515853c219340d6eb98",
+    "url": "/truss-solver/solver/static/js/runtime-main.701c12f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/truss-solver/precache-manifest.29c45669624a807b54fe013442903b93.js"
+  "/truss-solver/solver/precache-manifest.8954798f3ea4aaf2e2391e275d44d34a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/truss-solver/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/truss-solver/solver/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
