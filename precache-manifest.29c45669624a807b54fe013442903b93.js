da88cc0be790078e94b1ee2c747c9610932f2a87
John Richmond Escalona <johnrichmondescalona@gmail.com>
2020-11-03 16:22:10 -0800

    Updates

---
+++ precache-manifest.29c45669624a807b54fe013442903b93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea6999b48b04a2d6bf9a211a25235c14",
+    "url": "/truss-solver/index.html"
+  },
+  {
+    "revision": "ff5f923391fc4c4a7e59",
+    "url": "/truss-solver/static/css/main.66ba8ebf.chunk.css"
+  },
+  {
+    "revision": "6de6ff77019f89dcfb0b",
+    "url": "/truss-solver/static/js/2.35043cc4.chunk.js"
+  },
+  {
+    "revision": "fa3c65458c0dff53fb45377e9a84216e",
+    "url": "/truss-solver/static/js/2.35043cc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff5f923391fc4c4a7e59",
+    "url": "/truss-solver/static/js/main.d606c0a4.chunk.js"
+  },
+  {
+    "revision": "4a67ccbebcc3cf794d2e",
+    "url": "/truss-solver/static/js/runtime-main.14b7e4c5.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/truss-solver/precache-manifest.29c45669624a807b54fe013442903b93.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/truss-solver/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
